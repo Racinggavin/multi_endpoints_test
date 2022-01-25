6d5415b6edaf4b3040bba776948312abf5c42322
Gavin@Quandic <gavin.chang@quandic.com>
2022-01-25 15:19:57 +0800

    Add ferry graphql client for app2

---
+++ graphql/graphql_app2/lib/graphql_app2.dart
@@ -0,0 +1,6 @@
+library graphql_app2;
+
+// Test2
+export 'package:graphql_app1/graphql/test2.data.gql.dart';
+export 'package:graphql_app1/graphql/test2.req.gql.dart';
+export 'package:graphql_app1/graphql/test2.var.gql.dart';
